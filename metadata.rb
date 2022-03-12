a846b32c33bf7befbd4377025cf1b64851902bd8
R <RobertNorthard@users.noreply.github.com>
2022-03-12 20:42:03 +0000

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,4 @@
 name 'jar_deployment'
-maintainer 'robertnorthard'
-maintainer_email 'robertnorthard@googlemail.com'
 license 'All rights reserved'
 description 'Installs/Configures jar_deployment'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
